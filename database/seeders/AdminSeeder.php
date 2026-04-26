b9b52c30c25663ec77d4302b522f8e723a31c471
ailsalvn <email_ailsalvn@gmail.com>
2026-04-26 07:41:19 +0700

    first commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -10,14 +8,13 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-       User::updateOrCreate(
-    ['email' => 'admin@gmail.com'],
-    [
-        'name' => 'Admin',
-        'password' => Hash::make('12345678'),
-        'role' => 'admin',
-        'status' => 'approved',
-    ]
-);
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+            ]
+        );
     }
 }
--- database/seeders/AnggotaSeeder.php
+++ database/seeders/AnggotaSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -10,11 +8,13 @@ class AnggotaSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Anggota',
-            'email' => 'anggota@gmail.com',
-            'password' => Hash::make('12345678'),
-            'role' => 'anggota'
-        ]);
+        User::firstOrCreate(
+            ['email' => 'anggota@gmail.com'],
+            [
+                'name' => 'Anggota',
+                'password' => Hash::make('12345678'),
+                'role' => 'anggota',
+            ]
+        );
     }
 }

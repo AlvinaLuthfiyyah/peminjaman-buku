8584f62fa44b1aeeafc2cef8859ce0ff462d706f
ailsalvn <email_ailsalvn@gmail.com>
2026-04-26 07:18:06 +0700

    fixxing

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,35 +11,31 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
     public function create(): View
     {
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
+
         $user = Auth::user();
 
-if ($user->role !== 'admin' && strtolower(trim($user->status)) !== 'approved') {
-    Auth::logout();
-    return back()->with('error', 'Akun belum disetujui admin');
-}
+        if ($user->role !== 'admin' && strtolower(trim($user->status)) !== 'approved') {
+            Auth::logout();
+            return back()->with('error', 'Akun belum disetujui admin');
+        }
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if ($user->role === 'admin') {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect()->route('anggota.dashboard');
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();

f44609903492a5742b6c5128ebe0d10f3a6cf636
AlvinaLuthfiyyah <sgrvna1@gmail.com>
2026-04-26 18:04:41 +0700

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -77,7 +77,7 @@ class BookController extends Controller
     }
 
     // EDIT
-
+\Log::info('Update called', ['has_cover' => $request->hasFile('cover')]);
   public function update(Request $request, Book $book)
 {
     $validated = $request->validate([
